7c2db63e08f196c10d1bc5a86f2429a095d150c3
Emre Yeler <yeleremre@hotmail.com>
2024-03-02 15:41:10 +0300

    Collection yapısı eklendi.

--- RickandMortyApp-MVVM/Features/View/CharacterViewController.swift
+++ RickandMortyApp-MVVM/Features/View/CharacterViewController.swift
@@ -6,20 +6,28 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CharacterViewController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
-    
     //MARK: VARIABLES
+    @IBOutlet weak var collectionView: UICollectionView!
     private var characterViewModel: CharacterViewModelProtocol = CharacterViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         characterViewModel.delegate = self
         characterViewModel.fetchData()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        collectionView.register(UINib(nibName: "CharacterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+        
     }
 }
 extension CharacterViewController: CharacterViewModelOutputProtocol {
     func update() {
+        collectionView.reloadData()
         print(characterViewModel.characters!)
     }
     
@@ -28,3 +36,30 @@ extension CharacterViewController: CharacterViewModelOutputProtocol {
     }
 }
 
+extension CharacterViewController: UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return characterViewModel.characters?.results.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CharacterCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        if let char = characterViewModel.characters?.results[indexPath.row],
+           let imageURL = URL(string: char.image) {
+            DispatchQueue.main.async {
+                cell.cellImageView.kf.setImage(with: imageURL)
+                cell.cellLabel.text = char.name
+            }
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (collectionView.frame.size.width-10)/2
+        return CGSize(width: size, height: size)
+    }
+       
+}
--- RickandMortyApp-MVVM/Features/View/SubView/CharacterCollectionViewCell.swift
+++ RickandMortyApp-MVVM/Features/View/SubView/CharacterCollectionViewCell.swift
@@ -2,16 +2,20 @@
 //  CharacterCollectionViewCell.swift
 //  RickandMortyApp-MVVM
 //
-//  Created by Emre on 29.02.2024.
+//  Created by Emre on 2.03.2024.
 //
 
 import UIKit
 
 class CharacterCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    @IBOutlet weak var cellLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        layer.borderWidth = 1
+        layer.cornerRadius = 23
+        cellImageView.layer.cornerRadius = 23
     }
 
 }
