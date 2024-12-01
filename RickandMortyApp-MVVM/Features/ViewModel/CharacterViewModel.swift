df8bc2db60ffd3fa7dd11a9c8fd41055eb7bb5ae
Emre Yeler <yeleremre@hotmail.com>
2024-12-01 14:04:57 +0300

    ARC added.

--- RickandMortyApp-MVVM/Features/ViewModel/CharacterViewModel.swift
+++ RickandMortyApp-MVVM/Features/ViewModel/CharacterViewModel.swift
@@ -22,6 +22,10 @@ protocol CharacterViewModelOutputProtocol: AnyObject {
 final class CharacterViewModel {
     private(set) var characters: [Result] = []
     private(set) var nextPageUrl: String?
+    //CharacterViewController bir CharacterViewModel örneği oluşturur. Kendisini bu modelin delegate'i olarak atar
+    //CharacterViewModel, delegate'e güçlü bir referans (strong reference) tutar.
+    //CharacterViewController, zaten CharacterViewModel'in sahibi olduğu için, onu güçlü bir referans ile tutar.
+    //Burada retain cycle oluşur ve bunu çözmek için delegate'i weak olarak tanımlarız
     weak var delegate: CharacterViewModelOutputProtocol?
     
     func fetchData(filterByName name: String?) {
@@ -32,7 +36,10 @@ final class CharacterViewModel {
             parameters["name"] = name
             nextPageUrl = nil
         }
-        
+        //CharacterViewModel, bir closure'ı çalıştırmak üzere NetworkManager'e verir.
+        //Closure, self'e (yani CharacterViewModel'e) güçlü bir referans oluşturur.
+        //Eğer CharacterViewModel, NetworkManager tarafından tutulmaya devam ederse veya uzun süreli bir işlemi bekliyorsa, retain cycle oluşur.
+        //Bunu çözmek için NetworkManager'daki closure'ı weak olarak tanımlarız.
         if let url = URL(string: nextPageUrl ?? "\(RaMPath.CHAR.withBaseUrl())") {
             NetworkManager.shared.request(from: url, method: .get, parameters: parameters) { [weak self] (result: Swift.Result<Character, ErrorTypes>) in
                 switch result {
